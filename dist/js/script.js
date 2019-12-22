da33392028de56a92eab3de9ff9c47f5b9d6cce0
Yuliya Payevska <y.payevska@gmail.com>
2019-12-23 00:47:26 +0200

    doing responsive pages

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,10 +12,61 @@ $(document).ready(function(){
 
 //slider slick about-page
 	
-	$('.offer-slider').slick({
+	/*$('.offer-slider').slick({
 	  lazyLoad: 'ondemand',
 	  slidesToShow: 3,
 	  slidesToScroll: 1
-	});
+	  responsive: [{
+            breakpoint: 1500,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+
+        }, {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+            }
+        }
+        ]
+	});*/
+
+	$('.offer-slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1720,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 });
